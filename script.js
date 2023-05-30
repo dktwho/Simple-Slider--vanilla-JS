dc4d8fe64a57fd22ffebac96b1edb7607fb9ac8b
Alexander Korobov <dktwho@mail.ru>
2023-05-30 20:26:30 +0300

    create arr with url img, add event listener currentimg +1, -1

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const prevBtn = document.getElementById('show-prev-btn')
 const nextBtn = document.getElementById('show-next-btn')
 
+let images = []
+images.push('https://gratisography.com/wp-content/uploads/2023/05/gratisography-retro-pastime-free-stock-photo-800x525.jpg')
+images.push('https://gratisography.com/wp-content/uploads/2023/05/gratisography-cool-colorful-door-free-stock-photo-800x525.jpg')
+images.push('https://gratisography.com/wp-content/uploads/2023/05/gratisography-party-balloons-free-stock-photo-800x525.jpg')
 
+let currentImage = 0
+
+const slideImage = document.getElementById('slide-image')
+slideImage.src = images[currentImage]
 
 prevBtn.addEventListener('click', onShowPrevBtnClick )
 nextBtn.addEventListener('click', onShowNextBtnClick )
 
 
+
+
+
+
+
+
 function onShowPrevBtnClick() {
-  console.log('prev')
+  currentImage--
+  slideImage.src = images[currentImage]
 }
 
 function onShowNextBtnClick() {
-  console.log('next')
+  currentImage++
+  slideImage.src = images[currentImage]
 }
 
